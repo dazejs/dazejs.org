59d50c0eac9b6d883b98dac505eab8ba573af0e6
czewail <chanzewail@gmail.com>
2020-02-22 18:15:48 +0800

    add

--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -2,13 +2,17 @@ import * as path from 'path';
 import autoprefixer from 'autoprefixer';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import { Configuration } from 'webpack';
 
-const config = {
-  mode: process.env.NODE_ENV,
-  devtool: process.env.NODE_ENV === 'development' ? 'cheap-module-source-map' : false,
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+
+const config: Configuration = {
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'cheap-module-source-map' : false,
   entry: { 
-    app: ['./src/index.tsx']
+    app: ['../src/index']
   },
   output: {
     path: path.resolve(__dirname, '../dist/assets'),
@@ -16,28 +20,34 @@ const config = {
     publicPath: '/assets/'
   },
   module: {
+    // 文件中如果缺少exports时会直接报错而不是警告
+    strictExportPresence: true,
     rules: [
+      { parser: { requireEnsure: false } },
       {
-        test: /\.(j|t)sx?$/,
-        exclude: /node_modules/,
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: path.resolve(__dirname, '../src'),
         loader: 'babel-loader'
       },
       {
         test: /\.less$/,
         loader: [
-          {
+          isDev && 'style-loader',
+          isProd && {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              hmr: process.env.NODE_ENV === 'development',
+              hmr: true,
             },
           },
           'css-loader',
           {
             loader: 'postcss-loader',
+            ident: 'postcss',
             options: {
               sourceMap: 'inline',
               plugins: () => [
                 autoprefixer({
+                  flexbox: 'no-2009',
                   overrideBrowserslist: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4']
                 }),
                 // require('cssnano')()
@@ -66,15 +76,18 @@ const config = {
       filename: path.resolve(__dirname, '../dist/index.html'),
       template: path.resolve(__dirname, './tpl/index.html'),
       inject: true,
-      minify: {
+      minify: isProd ? {
         removeComments: true,
         collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      } : undefined,
     })
   ],
   resolve: {
@@ -108,5 +121,14 @@ const config = {
     ]
   }
 };
+
+if (isDev) {
+  config.devServer = {
+    contentBase: path.join(__dirname, '../dist'),
+    publicPath: '/assets/',
+    hot: true,
+    port: 9898
+  }
+}
 
 export default config;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<div>111</div>, document.getElementById('root'));
