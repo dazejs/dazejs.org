fdad8b1a2feea7f3fd521bedd6a908fbfd2691d1
czewail <chanzewail@gmail.com>
2020-03-01 14:39:38 +0800

    add

--- scripts/transport.sh
+++ scripts/transport.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-set -x
-tar -czvf ~/assets.tar.gz ./dist
-scp -i ./id_rsa ~/assets.tar.gz root@111.229.3.72:/home/webroot
+tar -czvf ./assets.tar.gz ./dist
+scp -i ./id_rsa ./assets.tar.gz root@111.229.3.72:/home/webroot
 ssh -i ./id_rsa root@111.229.3.72 'cd /home/webroot && tar -xzf assets.tar.gz && rm -f assets.tar.gz && docker restart zewail-nginx'
