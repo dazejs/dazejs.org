bf0ab86a42a11cee956ff374e507ef8c1e3214da
czewail <chanzewail@gmail.com>
2020-03-01 14:49:41 +0800

    add

--- scripts/rsa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-mkdir -p ~/.ssh
-rm -f ~/.ssh/known_hosts
-ssh-keyscan -t rsa 111.229.3.72 >> ~/.ssh/known_hosts
--- scripts/transport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-tar -czvf ./assets.tar.gz ./dist
-scp -i ./id_rsa ./assets.tar.gz root@111.229.3.72:/home/webroot
-ssh -i ./id_rsa root@111.229.3.72 'cd /home/webroot && tar -xzf assets.tar.gz && rm -f assets.tar.gz && docker restart zewail-nginx'
